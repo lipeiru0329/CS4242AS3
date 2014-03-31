ab9da933eaa37df643b056a72e2f671a254d0b53
How Wei Keng <admin@kengary.name>
2014-03-31 11:39:42 +0800

    More Crawls via SearchAPI

---
+++ WebApp/StreamCrawler.py
@@ -0,0 +1,53 @@
+import twitter
+import csv
+import twitter__login
+import json, time, sys, os
+'''
+STREAM API Sample
+'''
+consumer_key = '2JRLM23QHyLyBABuqg4tqQ'
+consumer_secret = 'avpoP356DDKbHtTRiicjKBC01yXqfaI8QCgfZebmjA'
+access_token = '20692466-UHvKF9lGGyjhkyn1H5PX55nXdGbZsCmGOZo6Exaoy'
+access_token_secret = 'FXEAFtjBvT9hRLxgPubBRUiv8sR7GTSpmVYoW9UArGLOi'
+  
+g_locations = "103.6271, 1.2427, 103.9993, 1.4548" #singapore 
+g_language = "en"
+g_geocode="1.3520830,103.8198360,100km"  #singapore radius
+
+class StreamCrawler(object):
+    def __init__(self):
+        self.counter = 0
+        self.fprefix = ""
+        self.output  = None
+        self.twitter_rest = twitter__login.login()
+        self.twitter_stream = twitter.TwitterStream(auth=twitter.oauth.OAuth(access_token, access_token_secret,consumer_key, consumer_secret))
+    
+    def streamAPI(self):
+        resultSet = self.twitter_stream.statuses.filter(locations = g_locations)
+        return resultSet
+    
+    def writeToFile(self, resultSet, aspect,keyword):
+        if not os.path.isdir("data/"+aspect+"/"):
+            os.mkdir("data/"+aspect+"/")
+        fname = "data/"+aspect+"/"+keyword+"-"+time.strftime('%Y%m%d-%H%M%S')+".json"
+        if os.path.isfile(fname):
+            fname = "data/"+aspect+"/"+keyword+"-"+time.strftime('%Y%m%d-%H%M%S')+".json"
+        f = open(fname,'w')
+        for r in resultSet:
+            f.write(str(r)+"\n")
+        f.close()
+        return
+    
+    def startStreamCrawling(self):
+        rs = self.streamAPI()
+        return
+    
+c = StreamCrawler()
+c.startStreamCrawling()
+    
+    
+    
+    
+    
+    
+    
--- WebApp/views.py
+++ WebApp/views.py
@@ -1,91 +1,11 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-import tweepy
-from tweepy import StreamListener
-import json, time, sys
 
-consumer_key = '2JRLM23QHyLyBABuqg4tqQ'
-consumer_secret = 'avpoP356DDKbHtTRiicjKBC01yXqfaI8QCgfZebmjA'
-access_token = '20692466-4kkQfaO8V0e2cVBDzfYg4EkFdQO9u0CNZLoP8Xma5'
-access_token_secret = '0bUGan28R0Dt2f0NIIjA2AcCkNUelANx674aWUH9Oj08f'
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
 # Create your views here.
 def index(request):
-    '''
-    l = StdOutListener()
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    stream = tweepy.Stream(auth, l)
-    stream.filter(track=['Singapore'])
-    '''
-    track  = ['SingaporeGP','F1 Race',]
-    locations = [103.61,1.22,104.01356,1.456674]
-    language = ['en']
-    follow = []
-    listen = SListener(api, 'test')
-    stream = tweepy.Stream(auth, listen)
-    print "Streaming started on %s users and %s keywords..." % (len(track), len(follow))
-    try: 
-        stream.filter(track = track, follow = follow, locations = locations)
-    except:
-        print "error!"
-        stream.disconnect()
+
     return render_to_response('index.html')
+    
 
-# Helper Class and methods
 
-class SListener(StreamListener):
-
-    def __init__(self, api = None, fprefix = 'streamer'):
-        self.api = api
-        self.counter = 0
-        self.fprefix = fprefix
-        self.output  = open('CS4242AppProject/data/F1/' + fprefix + '.' 
-                            + time.strftime('%Y%m%d-%H%M%S') + '.json', 'w')
-        self.delout  = open('CS4242AppProject/data/delete.txt', 'a')
-
-    def on_data(self, data):
-
-        if  'in_reply_to_status' in data:
-            self.on_status(data)
-        elif 'delete' in data:
-            delete = json.loads(data)['delete']['status']
-            if self.on_delete(delete['id'], delete['user_id']) is False:
-                return False
-        elif 'limit' in data:
-            if self.on_limit(json.loads(data)['limit']['track']) is False:
-                return False
-        elif 'warning' in data:
-            warning = json.loads(data)['warnings']
-            print warning['message']
-            return False
-        
-    def on_status(self, status):
-        self.output.write(status + "")
-        self.counter += 1
-        if self.counter >= 100:
-            self.output.close()
-            self.output = open('CS4242AppProject/data/F1/' + self.fprefix + '.' 
-                               + time.strftime('%Y%m%d-%H%M%S') + '.json', 'w')
-            self.counter = 0
-        return
-
-    def on_delete(self, status_id, user_id):
-        self.delout.write( str(status_id) + "")
-        return
-
-    def on_limit(self, track):
-        sys.stderr.write(track + "")
-        return
-
-    def on_error(self, status_code):
-        sys.stderr.write('Error: ' + str(status_code) + "")
-        return False
-
-    def on_timeout(self):
-        sys.stderr.write("Timeout, sleeping for 60 seconds...")
-        time.sleep(60)
-        return  
