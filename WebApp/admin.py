2bde00dd754b8303bb0599731c5b28e2682c6dc2
How Wei Keng <admin@kengary.name>
2014-04-06 23:36:18 +0800

    Reinit Setup for MongoDB support

--- WebApp/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from WebApp.models import SavedTweet
-# Register your models here.
-admin.site.register(SavedTweet)
--- WebApp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from jsonfield import JSONField
-
-# Create your models here.
-class SavedTweet(models.Model):
-    json = JSONField()
